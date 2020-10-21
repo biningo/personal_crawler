bd6f2f2cf15c41b6b6356b13ef9d7a614848809e
biningo <biningo@yeah.net>
2020-10-21 13:59:21 +0800

    get detail info

--- douban_book/book_detail.py
+++ douban_book/book_detail.py
@@ -5,4 +5,28 @@ def book_directory(url: str):
     resp = requests.get(url=url, headers=headers)
     selector = etree.HTML(resp.text)
     id = "dir_" + url.split('/')[-2] + "_full"
-    return '\n'.join(list(map(lambda x: x.strip().replace('\u3000','').replace('\t',''), selector.xpath("//div[@id='" + id + "']//text()")))[:-3])
+    return '\n'.join(list(map(lambda x: x.strip().replace('\u3000', '').replace('\t', ''),
+                              selector.xpath("//div[@id='" + id + "']//text()")))[:-3])
+
+#清理工具函数
+def clear_text(content):
+    return list(filter(lambda x: x != '', list(map(lambda x: x.strip(), content))))
+
+def book_info(url: str):
+    resp = requests.get(url=url, headers=headers)
+    selector = etree.HTML(resp.text)
+    titles = clear_text(selector.xpath("//div[@id='info']/span[@class='pl']/text()"))
+    titles = list(map(lambda x:x.replace(':',''),titles))
+    values = clear_text(selector.xpath("//div[@id='info']/text()"))
+    info = {}
+    for k,v in zip(titles, values):
+        info[k] = v
+    aSpan = selector.xpath("//div[@id='info']/span[not(@class)]")
+    for v in aSpan:
+        titles = v.xpath(".//span[@class='pl']//text()")
+        values = v.xpath(".//a//text()")
+        title = ''.join(titles).strip()
+        value = '/'.join(values).strip()
+        info[title] = value
+    info['star'] = ''.join(selector.xpath("//div[@id='interest_sectl']//strong/text()")).strip()
+    return info
